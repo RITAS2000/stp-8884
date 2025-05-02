03a6d2315c63773ba3a219becda6edcf8fd2bf99
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-05-02 04:44:57 +0300

    ingame

---
+++ src/js/ingame.js
@@ -0,0 +1,25 @@
+import Swiper from 'swiper';
+import { Navigation, Keyboard } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+Swiper.use([Navigation, Keyboard]);
+
+const swiper = new Swiper('.swiper-ingame', {
+  slidesPerView: 1,
+  spaceBetween: 20, // або як тобі треба
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  navigation: {
+    nextEl: '.arrow-right',
+    prevEl: '.arrow-left',
+  },
+  a11y: true,
+  breakpoints: {
+    1200: {
+      slidesPerView: 2.5,
+    },
+  },
+});
