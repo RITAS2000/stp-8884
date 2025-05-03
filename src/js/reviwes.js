ece6f75026d0439cee3be0cc1718fedf48932790
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-05-03 23:51:08 +0300

    stars

--- src/js/reviwes.js
+++ src/js/reviwes.js
@@ -1,3 +1,31 @@
+import imgStar from '../img/reviwes/star.png';
+
+const container = document.querySelector('.falling-stars');
+
+function createStarImage() {
+  const img = document.createElement('img');
+  img.src = imgStar;
+  console.log(img.src);
+  img.classList.add('star-image');
+  img.style.left = `${Math.random() * 100}vw`;
+  img.style.top = `-30px`;
+  img.style.animationDuration = `${Math.random() * 2 + 10}s`;
+  container.appendChild(img);
+  img.addEventListener('animationend', () => {
+    img.remove();
+  });
+}
+
+const starInterval = setInterval(createStarImage, 600);
+
+function stopFallingStars() {
+  clearInterval(starInterval);
+  document.removeEventListener('click', stopFallingStars);
+
+  document.querySelectorAll('.star-image').forEach(star => star.remove());
+}
+document.addEventListener('click', stopFallingStars);
+
 const reviwesItems = document.querySelectorAll('.rewivse-item');
 const section = document.querySelector('#reviwes');
 
