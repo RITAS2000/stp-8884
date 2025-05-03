202e42b85c851a23202aee06e03f17be12375927
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-05-03 15:26:55 +0300

    anime

---
+++ src/js/battle.js
@@ -0,0 +1,22 @@
+const imageGirl = document.querySelector('.battle-girl');
+const section = document.querySelector('#battle');
+const imageBoy = document.querySelector('.battle-boy');
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        imageGirl.classList.add('visible');
+        setTimeout(() => {
+          imageBoy.classList.add('visible');
+        }, 1500);
+        observer.disconnect();
+      }
+    });
+  },
+  {
+    threshold: 0.3,
+  }
+);
+
+observer.observe(section);
